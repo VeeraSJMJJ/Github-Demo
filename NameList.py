a23e28d980ed6760d5b385385de6f2c975432365
VeeraSJMJJ <veeramanim7@gmail.com>
2025-07-12 18:31:36 +0530

    Python Challenge Day 10 Task Completed

---
+++ NameList.py
@@ -0,0 +1,6 @@
+# Store 5 names in a list
+names = ["Veera", "GenAI Architect", "Social Eagle AI", "Social Eagle Community", "Fly High"]
+
+# Loop through the list and print each name with its length
+for name in names:
+    print(f"Name: {name}, Length: {len(name)}")
